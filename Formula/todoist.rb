3baad88cfdb3ed549556d822bdabe390f0272570
Takumasa Sakao <sakataku7@gmail.com>
2019-08-16 00:25:31 +0900

    Modify invalid path

--- Formula/todoist.rb
+++ Formula/todoist.rb
@@ -2,7 +2,7 @@ class Todoist < Formula
   desc "Todoist CLI client"
   homepage "https://github.com/sachaos/todoist"
 
-  url "https://github.com/sachaos/todoist/archive/v0.14.0-homebrew.tar.gz"
+  url "https://github.com/sachaos/todoist/archive/v0.14.0.tar.gz"
   sha256 "c58dd771f7b1a2750f8805561aa1e8549a7c395fdeab16cac66c6d5735936e99"
   head "https://github.com/sachaos/todoist.git"
 
