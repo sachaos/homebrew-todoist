38b7558d3a5a6a6b0f33183eb4b22c7ad9a68116
Takumasa Sakao <tsakao@zlab.co.jp>
2023-01-14 01:07:34 +0900

    Fix version

--- Formula/todoist.rb
+++ Formula/todoist.rb
@@ -2,7 +2,7 @@ class Todoist < Formula
   desc "Todoist CLI client"
   homepage "https://github.com/sachaos/todoist"
 
-  url "https://github.com/sachaos/todoist/archive/v0.17.0.tar.gz"
+  url "https://github.com/sachaos/todoist/archive/v0.18.0.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   head "https://github.com/sachaos/todoist.git"
 
