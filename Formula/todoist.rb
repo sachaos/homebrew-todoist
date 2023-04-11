4d9f72d505f22b27d43cfac1b0280ad7f238bc46
Takumasa Sakao <tsakao@zlab.co.jp>
2023-04-11 23:33:11 +0900

    Update to 0.19.0

--- Formula/todoist.rb
+++ Formula/todoist.rb
@@ -2,8 +2,8 @@ class Todoist < Formula
   desc "Todoist CLI client"
   homepage "https://github.com/sachaos/todoist"
 
-  url "https://github.com/sachaos/todoist/archive/v0.18.0.tar.gz"
-  sha256 "2cc89bdf44d5a9bda6f748069cf8841e10e3b0c14dd4ddb17d5b1f7a7bc7d759"
+  url "https://github.com/sachaos/todoist/archive/v0.19.0.tar.gz"
+  sha256 "acf91e168abe7d251a68ae87b1943a1f83ef3b7fd89e4f56bd82cb3db51b6ade"
   head "https://github.com/sachaos/todoist.git"
 
   depends_on "go" => :build
